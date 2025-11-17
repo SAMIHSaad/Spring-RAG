fc1eaf8045683b1e2fa81b9e7f4db78d619dd331
SaadSAMIH <saadsamih23@gmail.com>
2025-11-17 09:54:13 +0100

    screen ajoute

--- src/main/java/ma/emsi/samih/springrag/SpringRagApplication.java
+++ src/main/java/ma/emsi/samih/springrag/SpringRagApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringRagApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringRagApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRagApplication.class, args);
+    }
 
 }
